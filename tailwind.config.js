fbec0cf60f1c914f3eb85aa0355c412a4bab2a7a
Mark Small <mark.small@telespazio.com>
2024-07-08 12:14:12 +0100

    fix(project): Install and configure tools used by tailwindcss
    I installed postcss and autoprefixer dependencies, used by tailwind.
    I also added configs for them and updated the wider typescript and
    vite configs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: ['rounded-sm', 'rounded-md', 'rounded-lg', 'rounded-full'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#84c98c',
+          200: '#6ec079',
+          300: '#56b665',
+          400: '#39ad51',
+          500: '#00a33b',
+          600: '#008d32',
+          700: '#007829',
+          800: '#006321',
+          900: '#004f19',
+        },
+        secondary: {
+          100: '#90afda',
+          200: '#7ea2d3',
+          300: '#6c95cc',
+          400: '#5a88c5',
+          500: '#487bbe',
+          600: '#3d6aa5',
+          700: '#335a8c',
+          800: '#294a75',
+          900: '#1f3a5e',
+        },
+        success: '#4caf50',
+        warning: '#ff9800',
+        error: '#f44336',
+        info: '#2196f3',
+      },
+    },
   },
   plugins: [],
-}
-
+};
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,48 @@
-/// <reference types="vitest" />
-
 import * as path from 'path';
 
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { peerDependencies } from './package.json';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: './src/index.ts', // Specifies the entry point for building the library.
-      name: 'dsp-styled-component-library', // Sets the name of the generated library.
+      entry: path.resolve(__dirname, './src/index.ts'), // Specifies the entry point for building the library.
+      name: '@mark-small/tailwind-component-library', // Sets the name of the generated library.
       fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
       formats: ['cjs', 'es'], // Specifies the output formats (CommonJS and ES modules).
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      external: ['react', 'react-dom', 'tailwindcss'], // Defines external dependencies for Rollup bundling.
+      // external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss',
+        },
+      },
     },
     sourcemap: true, // Generates source maps for debugging.
     emptyOutDir: true, // Clears the output directory before building.
   },
-  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  plugins: [react(), dts({ rollupTypes: true })], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Specifies alias for importing components.
+      '@': path.resolve(__dirname, './src'), // Specifies alias for importing components.
     },
   },
   test: {
     globals: true, // Enables global testing for Vite.
     environment: 'jsdom', // Configures the test environment to run in a browser-like environment.
-    setupFiles: [path.resolve(__dirname, './setupTests.ts')], // Specifies the setup file for running tests.
+    setupFiles: './setupTests.ts', // Specifies the setup file for running tests.
+    // setupFiles: [path.resolve(__dirname, './setupTests.ts')], // Specifies the setup file for running tests.
     include: ['src/**/*.test.{ts,tsx}'], // Specifies the files to be included for testing.
     exclude: ['node_modules/**'], // Specifies the files to be excluded from testing.
   },
